ac9460d2322b46f3431e517de46ed6d0eb65f993
shiv <shivendrathakursst@gmail.com>
2024-08-18 22:21:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header"
+import Content from "./components/content"
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Content/>
+      <h1 className="text-8xl">Here we gooo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full bg-slate-900 text-center py-5'>       
+        <h1 className='font-serif text-white text-3xl font-bold'>Movie XPlore</h1>
+    </div>
+  )
+}
+
+export default Header
